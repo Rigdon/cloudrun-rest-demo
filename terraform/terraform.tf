45859d9bea5a00daa0dd55723db6c53aaca9e268
Ryan Rigdon <mr.rigdon@gmail.com>
2022-12-19 20:10:22 -0500

    add initial test terraform files, including GitHub Action with caching

---
+++ terraform/terraform.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.4"
+    }
+  }
+  required_version = "~> 1.3"
+}
+
+provider "random" {}
